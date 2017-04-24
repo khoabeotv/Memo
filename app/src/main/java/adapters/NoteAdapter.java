b574fcbc42c9eb7ac2272b19494a0e21ed3efe36
DESKTOP-7OCE12I\KhoaBeo <khoatv141001@gmail.com>
2017-04-24 14:41:40 +0700

    fix animation listview

--- app/src/main/java/adapters/NoteAdapter.java
+++ app/src/main/java/adapters/NoteAdapter.java
@@ -23,62 +23,59 @@ import teambandau.memo.R;
 
 public class NoteAdapter extends BaseAdapter {
 
-    private List<Note> notes;
-    private LayoutInflater inflater;
-    private boolean[] animationStates;
-    Context context;
-    public NoteAdapter(Context context, List<Note> memos) {
-        this.context = context;
-        this.notes = memos;
-        this.inflater = LayoutInflater.from(context);
+  private List<Note> notes;
+  private LayoutInflater inflater;
+  private Context context;
 
+  public NoteAdapter(Context context, List<Note> memos) {
+    this.context = context;
+    this.notes = memos;
+    this.inflater = LayoutInflater.from(context);
+  }
+
+  @Override
+  public int getCount() {
+    return notes.size();
+  }
+
+  @Override
+  public Object getItem(int position) {
+    return notes.get(position);
+  }
+
+  @Override
+  public long getItemId(int position) {
+    return position;
+  }
+
+  @Override
+  public View getView(int position, View view, ViewGroup parent) {
+
+    view = inflater.inflate(R.layout.note_item, null, false);
+    TextView tvTitle = (TextView) view.findViewById(R.id.tvTitle);
+    TextView tvContent = (TextView) view.findViewById(R.id.tvContent);
+
+    View icon_v = view.findViewById(R.id.icon_v);
+    GradientDrawable bgShape = (GradientDrawable) icon_v.getBackground();
+
+    // TODO :
+    int idColor = context.getResources().getIdentifier(notes.get(position).getColor(), "color", context.getPackageName());
+    bgShape.setColor(context.getResources().getColor(idColor));
+
+    int typeAnim;
+    if (MainFragment.isBack) {
+      typeAnim = R.anim.left_to_right;
+    } else {
+      typeAnim = R.anim.right_to_left;
     }
+    Animation animation = AnimationUtils.loadAnimation(parent.getContext(), typeAnim);
+    animation.setStartOffset(0);
+    view.startAnimation(animation);
 
-    @Override
-    public int getCount() {
-        return notes.size();
-    }
 
-    @Override
-    public Object getItem(int position) {
-        return notes.get(position);
-    }
+    tvTitle.setText(notes.get(position).getTitle());
+    tvContent.setText(notes.get(position).getContent());
 
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public View getView(int position, View view, ViewGroup parent) {
-        animationStates = new boolean[notes.size()];
-        view = inflater.inflate(R.layout.note_item, null, false);
-        TextView tvTitle = (TextView) view.findViewById(R.id.tvTitle);
-        TextView tvContent = (TextView) view.findViewById(R.id.tvContent);
-
-        View icon_v = view.findViewById(R.id.icon_v);
-        GradientDrawable bgShape = (GradientDrawable)icon_v.getBackground();
-
-        // TODO :
-        int idColor  = context.getResources().getIdentifier(notes.get(position).getColor(), "color", context.getPackageName());
-        bgShape.setColor(context.getResources().getColor(idColor));
-
-        if (!animationStates[position]) {
-            animationStates[position] = true;
-            int typeAnim;
-            if (MainFragment.isBack) {
-                typeAnim = R.anim.left_to_right;
-            } else {
-                typeAnim = R.anim.right_to_left;
-            }
-            Animation animation = AnimationUtils.loadAnimation(parent.getContext(), typeAnim);
-            animation.setStartOffset(0);
-            view.startAnimation(animation);
-        }
-
-        tvTitle.setText(notes.get(position).getTitle());
-        tvContent.setText(notes.get(position).getContent());
-
-        return view;
-    }
+    return view;
+  }
 }
